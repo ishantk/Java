62c7c352c81e0a687ce337bc8a96967e1a96b6d1
Ishant Kumar <er.ishant@gmail.com>
2017-04-04 23:45:25 +0530

    InitialCommit

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+	public static void main(String[] args) {
+
+		// Print a line on console...
+		System.out.println("Hello World..");
+		System.out.println("Be Exceptional...");
+		
+		byte age = 10;
+		int someNum = 100000;
+		
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
+
+	}
+
+}
